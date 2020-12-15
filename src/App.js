fb53cd6b13eabc76d7287621db1ffbca372912c7
mrshuhrat <shuhratadm1n@gmail.com>
2020-12-15 16:36:06 +0500

    inittial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Itodos from './components/itodos/Itodos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  return Itodos()
 }
 
-export default App;
+export default App
---
+++ src/components/itodos/Itodos.js
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import './Itodos.css';
+
+
+function Itodos() {
+
+  const [todos, setTodos] = useState([])
+  const [i, setI] = useState(0)
+
+  const createTodo = e => {
+    if (e.target.value.length > 2 && e.keyCode === 13) {
+      setI(lastI => lastI + 1)
+      const newTodo = {
+        id: i,
+				title: e.target.value.trim(),
+				completed: false,
+      }
+      setTodos([...todos, newTodo])
+
+      e.target.value = null
+    }
+
+  }
+
+  const deleteTodo = (todo) => setTodos(todos.filter(t => t.id !== todo.id))
+  /* const complated = (id, evt) => (
+    function () {
+      todos.map(todo=> {
+        if (todo.id === id) {
+          todo.complated = !todo.complated
+        }
+        console.log(todos[0]);
+        return todo
+      })
+    }
+  ) */
+  
+  const nimadir = (e) => {
+    console.log();
+    if (e.target.checked) {
+      e.target.parentElement.classList.add('complated')
+    } else {
+      e.target.parentElement.classList.remove('complated')
+    }
+  }
+
+  return (
+    <>
+      <h1 className="page-title">itodos</h1>
+
+      <div className="box">
+        <input className="todos-input" type="text" onKeyUp={createTodo} placeholder="What needs to be done?" />
+        <ul className="todos">
+          {
+            todos.map(todo => {
+              return (
+                <li className="todo" key={todo.id}>
+                  <input className="todo__checkbox" type="checkbox" checked={todos.completed} onChange={nimadir} />
+                  <p className="todo.completed ? 'complated': null">{todo.title}</p>
+                  <button className="todo__button" onClick={deleteTodo.bind(null, todo)}>X</button>
+              </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </>
+    )
+}
+
+export default Itodos
